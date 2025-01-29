b4b555b2f63f74700ffbaa0bcedf52b32c67735a
Ivam Carlos <ivam.master10@gmail.com>
2025-01-29 17:42:57 -0300

    Change background.js

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,10 +1,23 @@
+/*
+Altere o array abaixo com seus links socias
+
+[0] Linkedin
+[1] GitHub
+[2] Youtube
+[3] Instagram
+[4] Twitter/X
+[5] Facebook
+
+Se não tiver link social de alguma rede social é só passar string vázia ""
+*/
+
 let links = [
-    "https://linkedin.com/in/ivam-carlos-aa152a99",
-    "https://github.com/IvamCarlos",
-    "https://www.youtube.com/@ivanzeragameplays",
-    "https://www.instagram.com/ivamcarlos10/",
-    "https://twitter.com/ivanzerafn",
-    ""
+    "sua_url_do_linkedin_aqui",
+    "sua_url_do_github_aqui",
+    "sua_url_do_youtube_aqui",
+    "sua_url_do_instagram_aqui",
+    "sua_url_do_twitter_aqui",
+    "sua_url_do_facebook_aqui"
 ];
 
 chrome.runtime.onInstalled.addListener(() => {
